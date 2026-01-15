44b24eed4a5fec93d3982dde04572528ca8abdc3
ssevindik <Samethub732@gmail.com>
2026-01-15 07:01:55 +0300

    feat(core): add components barrel export

---
+++ core/components/index.ts
@@ -0,0 +1,14 @@
+/**
+ * WireLang Core - Components Index
+ * Re-exports all component classes and factory functions
+ */
+
+export { Resistor, R } from './Resistor';
+export { Capacitor, C } from './Capacitor';
+export { Inductor, L } from './Inductor';
+export { Diode, D, type DiodeParams } from './Diode';
+export { LED, createLED, RED, GREEN, BLUE, YELLOW, WHITE, ORANGE, type LEDParams } from './LED';
+export type { LEDColor } from './LED';
+export { VoltageSource, DC, AC, type VoltageSourceParams } from './VoltageSource';
+export { CurrentSource, I_DC, I_AC, type CurrentSourceParams } from './CurrentSource';
+export { Ground, GND } from './Ground';
