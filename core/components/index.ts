289de7dbddd568e4b5a4484bc0f204836df6a883
ssevindik <Samethub732@gmail.com>
2026-01-15 09:27:41 +0300

    refactor: update component exports for Color enum

--- core/components/index.ts
+++ core/components/index.ts
@@ -7,7 +7,7 @@ export { Resistor, R } from './Resistor';
 export { Capacitor, C } from './Capacitor';
 export { Inductor, L } from './Inductor';
 export { Diode, D, type DiodeParams } from './Diode';
-export { LED, createLED, RED, GREEN, BLUE, YELLOW, WHITE, ORANGE, type LEDParams } from './LED';
+export { LEDComponent, LED, createLED, RED, GREEN, BLUE, YELLOW, WHITE, ORANGE, type LEDParams } from './LED';
 export type { LEDColor } from './LED';
 export { VoltageSource, DC, AC, type VoltageSourceParams } from './VoltageSource';
 export { CurrentSource, I_DC, I_AC, type CurrentSourceParams } from './CurrentSource';
