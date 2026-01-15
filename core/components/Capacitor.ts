712054fc33b217d45658252417e2e5b90c374b45
ssevindik <Samethub732@gmail.com>
2026-01-15 07:01:13 +0300

    feat(core): add Capacitor component

---
+++ core/components/Capacitor.ts
@@ -0,0 +1,44 @@
+/**
+ * WireLang Core - Capacitor Component
+ */
+
+import { TwoTerminalComponent } from '../Component';
+import { ComponentType } from '../types';
+import { formatWithUnit } from '../units';
+
+export class Capacitor extends TwoTerminalComponent {
+  constructor(capacitance: number) {
+    super(ComponentType.Capacitor, {
+      value: capacitance,
+      unit: 'F',
+    });
+  }
+
+  /**
+   * Get capacitance value in Farads
+   */
+  get capacitance(): number {
+    return this.params.value;
+  }
+
+  validate(): string[] {
+    const errors = super.validate();
+    
+    if (this.params.value === 0) {
+      errors.push('Capacitor: Capacitance cannot be zero');
+    }
+    
+    return errors;
+  }
+
+  toString(): string {
+    return `Capacitor(${formatWithUnit(this.capacitance, 'F')})`;
+  }
+}
+
+/**
+ * Factory function for DSL usage
+ */
+export function C(capacitance: number): Capacitor {
+  return new Capacitor(capacitance);
+}
