b8ad4c16b9ef18094860d10e84d3cb59ba7f9201
ssevindik <Samethub732@gmail.com>
2026-01-15 07:01:21 +0300

    feat(core): add Inductor component

---
+++ core/components/Inductor.ts
@@ -0,0 +1,44 @@
+/**
+ * WireLang Core - Inductor Component
+ */
+
+import { TwoTerminalComponent } from '../Component';
+import { ComponentType } from '../types';
+import { formatWithUnit } from '../units';
+
+export class Inductor extends TwoTerminalComponent {
+  constructor(inductance: number) {
+    super(ComponentType.Inductor, {
+      value: inductance,
+      unit: 'H',
+    });
+  }
+
+  /**
+   * Get inductance value in Henrys
+   */
+  get inductance(): number {
+    return this.params.value;
+  }
+
+  validate(): string[] {
+    const errors = super.validate();
+    
+    if (this.params.value === 0) {
+      errors.push('Inductor: Inductance cannot be zero');
+    }
+    
+    return errors;
+  }
+
+  toString(): string {
+    return `Inductor(${formatWithUnit(this.inductance, 'H')})`;
+  }
+}
+
+/**
+ * Factory function for DSL usage
+ */
+export function L(inductance: number): Inductor {
+  return new Inductor(inductance);
+}
